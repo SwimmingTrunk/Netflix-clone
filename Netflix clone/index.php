4ea045d9d1fe6803ef8d44a65ee31520f3062997
SwimmingTrunk <nikartiel@gmail.com>
2026-02-04 14:28:15 +0100

    Nik
    Added all the files, fixed up everything. Add the basic front end in the php

---
+++ Netflix clone/index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Placeholder</title>
+    <link rel="stylesheet" href="Css/style.css">
+</head>
+<body>
+    <!-- Navbar header -->
+    <header class="navbar">
+        <h1>NAME</h1>
+        <nav>
+            <a href="login.php"></a>
+            <a href="admin.php"></a>
+        </nav>
+    </header>
+
+    <div class="video-card">
+    <img src="images/" alt="">
+    </div>
+    <script src="Js/script.js"></script>
+</body>
+</html>
